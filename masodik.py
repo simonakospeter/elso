cfd74fa21be310df7729926fb851ce86efbdfddf
simonakospeter <simonakospeter@gmail.com>
2026-09-20 19:00:33 +0200

    masodik szinkron

--- masodik.py
+++ masodik.py
@@ -1,1 +1,14 @@
-print('Szia')
+felhasznalo_kora = int(15.65)
+felhasznalo_kora=int(input("Hány éves vagy: "))
+felhasznalo_kora += 1
+felhasznalo_neve = input('Kérem a nevet:')
+felhasznalo_kora *= 2
+metszet = felhasznalo_neve[:-5]
+jegyek = [2, 5, 4, 3]
+jegyek += [5]
+del jegyek[0]
+halmaz = {'magyar','angol','orosz', 3}
+hallgato = {"nev": 'Jolán', "kor": 19}
+print(hallgato["nev"])
+print(halmaz)
+print('Szia',felhasznalo_neve, "!", jegyek)
